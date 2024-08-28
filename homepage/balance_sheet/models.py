896e6b53254620855966485406e1630084e223d5
Taiichi Nakanishi (desktop) <taiichimail@gmail.com>
2024-08-28 21:47:05 +0900

    aaa

--- homepage/balance_sheet/models.py
+++ homepage/balance_sheet/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from category.models import Category
+from accounts.models import User
+import uuid
 # Create your models here.
+
+class Transaction(models.Model):
+    class Meta:
+        db_table = 'transaction'
+    
+    transaction_id = models.UUIDField(verbose_name="取引Id", default=uuid.uuid4, primary_key=True, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.TextField(verbose_name = "取引名", null = True, blank = True)
+    amount = models.IntegerField(verbose_name = "金額")
+    register_time = models.DateTimeField(verbose_name = "登録日時", auto_now_add = True)
+    update_time = models.DateTimeField(verbose_time = "更新日時", auto_now=True)
+    remark = models.TextField(verbose_name = "備考", blank = True, null = True)
--- homepage/category/models.py
+++ homepage/category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 from accounts.models import User
 
-class Category(models.Manager):
+class Category(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=32, null=False)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
